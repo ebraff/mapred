ba998805ad23ad0de960bbab35844e6821140e4e
Alexander DeOliveira <deoalex@builder.cs.rutgers.edu>
2014-03-26 18:53:59 -0400

    workgit add *

---
+++ MapReduce.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int aFlag; /*wordcount = 0, sort = 1*/
+int iFlag; /*threads = 0, procs = 1*/
+int numMapThreads;
+int numReduceThreads;
+char *infile, *outfile;
+
+void printUsage();
+int isNumber(char *string);
+int parseArgs(int argc, char *argv[]);
